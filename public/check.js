475cb5f10ecbeae49112ace29ffb9af1471c3acd
fahima101 <80126918+fahima101@users.noreply.github.com>
2021-05-03 17:55:42 -0400

    Update check.js

--- public/check.js
+++ public/check.js
@@ -10,6 +10,7 @@ function check(){
 	var question8 = document.quiz.question8.value;
 	var question9 = document.quiz.question9.value;
 	var question10 = document.quiz.question10.value;
+
 	var correct = 0;
 
 
@@ -25,34 +26,92 @@ function check(){
 
 	if (question4 == "96") { //Question 4's answer
 		correct++; //If got correct increases score by 1
+
 	}
+
+	if (question5 == "Clark Kent") { //Question 5's answer
+	correct++; //If got correct increases score by 1
+
+	}
+
+	if (question6 == "Dodgeball") { //Question 6's answer
+		correct++; //If got correct increases score by 1
+
+	}
+	if (question7 == "Silence") { //Question 7's answer
+		correct++; //If got correct increases score by 1
+
+	}
+	if (question8 == "A Holly Month") { //Question 8's answer
+		correct++; //If got correct increases score by 1
+
+	}
+	if (question9 == "1977") { //Question 9's answer
+		correct++; //If got correct increases score by 1
+	}
+
+	if (question10 == "baby") { //Question 10's answer
+		correct++; //If got correct increases score by 1
+	}
+	
+
+
+
+
+
+
+
 	//check.js into public 
 	//include in script tag
 	
 	//var pictures = ["public/win.gif", "public/meh.jpeg", "public/lose.gif"]; //Images based on their score
-	var messages = ["WOW! You're Smart", "So close", "I have seen better", "Go back to school","Try Harder"]; //The feedback after their 
+	var messages = ["WOW! You're Smart", "So close", "I have seen better", "Go back to school","Try Harder","Try Harder","Try Harder","Try Harder","Try Harder","Try Harder","Try Harder", "Try Harder"]; //The feedback after their 
 	//scores are revealed
 	var score;
 
 	//If statements to tally the users score
-	if (correct == 4) {
+	
+	
+	if (correct == 10) {
 		score = 0;
 	}
 
-	if (correct == 3) {
+	if (correct == 9) {
 		score = 1;
 	}
 
-	if (correct == 2) {
+	if (correct == 8) {
 		score = 2;
 	}
 
-	if (correct == 1) {
+	if (correct == 7) {
 		score = 3;
 	}
-	if(correct == 0) {
+	if(correct == 6) {
 		score = 4;
 	}
+
+	if (correct == 5) {
+		score = 5;
+	}
+
+	if (correct == 4) {
+		score = 6;
+	}
+
+	if (correct == 3) {
+		score = 7;
+	}
+	if (correct == 2) {
+		score = 8;
+	}
+	if (correct == 1) {
+		score = 9;
+	}
+	if (correct == 0) {
+		score = 10;
+	}
+	
 
 
 
