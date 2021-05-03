d46eb89e6032cf64c17d8c91db4323a801e8106d
hoodti33-t <77997616+hoodti33-t@users.noreply.github.com>
2021-05-03 15:51:16 -0400

    Update check.js

--- public/check.js
+++ public/check.js
@@ -4,6 +4,12 @@ function check(){
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
 	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
+	var question7 = document.quiz.question7.value;
+	var question8 = document.quiz.question8.value;
+	var question9 = document.quiz.question9.value;
+	var question10 = document.quiz.question10.value;
 	var correct = 0;
 
 
